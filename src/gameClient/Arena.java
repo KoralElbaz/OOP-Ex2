58a3a8390d93fbd062c156a129326cfb6421f88c
sivanyahav <sivanyhv1311@outlook.co.il>
2020-12-20 13:56:19 +0200

    update

--- src/gameClient/Arena.java
+++ src/gameClient/Arena.java
@@ -35,11 +35,11 @@ public class Arena {
 		_info = new ArrayList<String>();
 		this.time="";
 	}
-	private Arena(directed_weighted_graph g, List<CL_Agent> r, List<CL_Pokemon> p) {
-		_gg = g;
-		this.setAgents(r);
-		this.setPokemons(p);
-	}
+	//	private Arena(directed_weighted_graph g, List<CL_Agent> r, List<CL_Pokemon> p) {
+//		_gg = g;
+//		this.setAgents(r);
+//		this.setPokemons(p);
+//	}
 	public void setPokemons(List<CL_Pokemon> f)
 	{
 		this._pokemons = f;
@@ -77,12 +77,8 @@ public class Arena {
 	public directed_weighted_graph getGraph() {
 		return _gg;
 	}
-	public List<String> get_info() {
-		return _info;
-	}
-	public void set_info(List<String> _info) {
-		this._info = _info;
-	}
+
+
 
 	////////////////////////////////////////////////////
 	public static List<CL_Agent> getAgents(String aa, directed_weighted_graph gg) {
@@ -101,6 +97,18 @@ public class Arena {
 		}
 		return ans;
 	}
+
+	//	public static String gameLevel(String l){
+//		JSONObject str = null;
+//		try {
+//			JSONObject t=new JSONObject(l);
+//			str=t.getJSONObject("game_level");
+//		}
+//		catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+//		return str;
+//	}
 	public static ArrayList<CL_Pokemon> json2Pokemons(String fs) {
 		ArrayList<CL_Pokemon> ans = new  ArrayList<CL_Pokemon>();
 		try {
@@ -188,4 +196,18 @@ public class Arena {
 	}
 	public String getTime(){return time;}
 
+
+	public void set_info(String s, int id) {
+		if(_info.size()!=id){
+			_info.remove(id);
+		}
+		_info.add(id,s);
+	}
+
+	public List<String> get_info() {
+		return _info;
+	}
+	public void set_info(List<String> _info) {
+		this._info = _info;
+	}
 }
